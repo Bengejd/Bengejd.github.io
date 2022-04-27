78279ffa828ed366cfea1f391b47d3ee830c7037
Jordan Benge <Bengejd@miamioh.edu>
2022-04-27 11:28:34 -0500

    checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarCardComponent } from './components/sidebar-card/sidebar-card.component';
+import { SectionCardComponent } from './components/section-card/section-card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { ProjectExperienceComponent } from './components/project-experience/project-experience.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarCardComponent
+    SidebarCardComponent,
+    SectionCardComponent,
+    HeaderComponent,
+    WorkExperienceComponent,
+    ProjectExperienceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
